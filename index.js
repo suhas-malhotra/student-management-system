fe5f0b20cdb959a1dbd275dd1014194ada5d05a4
suhas-malhotra <xyzcyz00@gmail.com>
2022-06-08 12:13:56 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// All Routes
+app.use("/", require("./routes"));
+
+//Default Route
+app.get("/", (req, res) => {
+  return res.status(200).json({ working: true });
+});
+
+app.get("/*", (req, res) => {
+  return res.status(401).json({ message: "Route does not exist :(" });
+});
+const port = 80 || process.env.PORT;
+
+app.listen(port, () => {
+  console.log("Server started at port", port);
+});
